55bc4d5ab1db492f4100a56fe466a8c2540c7f64
Sandra Yun <sandramayla@gmail.com>
2022-08-30 17:11:30 -0400

    commit tree 232fd74abaaae0374b0213c47d1fe8832a6c1c1e

--- src/DogPics.js
+++ src/DogPics.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
+const baseURL = "https://dog.ceo/api/breeds/image/random/3"
+
 function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
+    fetch(baseURL)
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.message);
       });
-  });
+  }, []);;
 
   console.log("render");
 
